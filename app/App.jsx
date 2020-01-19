782c0f9e065f1283cdcecb5774c2a49a08e6d36b
Tjen Wellens <tjen.wellens@gmail.com>
2020-01-19 12:41:49 +0100

    4. Routing

--- app/App.jsx
+++ app/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import Home from './pages/Home'
-import {GlobalStyles} from "./styles";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import importComponent from 'react-imported-component';
+import Home from './pages/Home.jsx'
+import LoadingComponent from './pages/Loading'
+import ErrorComponent from './pages/Error'
+const About = importComponent(() => import("./pages/About"), {
+    LoadingComponent,
+    ErrorComponent
+});
 const App = () => (
     <React.Fragment>
         <GlobalStyles />
         <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/about" render={() => <About />} />
             <Redirect to="/" />
         </Switch>
     </React.Fragment>
