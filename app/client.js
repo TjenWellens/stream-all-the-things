34e57ca6ca2cf05694e65c294af956358ae2cd16
Tjen Wellens <tjen.wellens@gmail.com>
2020-01-19 11:22:01 +0100

    2. Development mode with parcel

---
+++ app/client.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {HelmetProvider} from 'react-helmet-async';
+
+const element = document.getElementById('app')
+const app = (
+    <HelmetProvider>
+        <App/>
+    </HelmetProvider>
+)
+ReactDOM.render(app, element)
+{
+    //Enable Hot Module Reloading
+    if (module.hot) {
+        module.hot.appect();
+    }
+}
