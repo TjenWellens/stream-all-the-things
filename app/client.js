f9e62822cf8455344bb59f30752629902121c791
Tjen Wellens <tjen.wellens@gmail.com>
2020-01-19 12:55:50 +0100

    6. Streaming Server Side Rendering

--- app/client.js
+++ app/client.js
@@ -4,6 +4,8 @@ import App from './App'
 import {HelmetProvider} from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom'
 import {GlobalStyles} from "./styles";
+import { rehydrateMarks } from 'react-imported-component';
+import importedComponents from './imported'; // eslint-disable-line
 
 const element = document.getElementById('app')
 const app = (
@@ -14,7 +16,16 @@ const app = (
         </BrowserRouter>
     </HelmetProvider>
 )
-ReactDOM.render(app, element)
+// In production, we want to hydrate instead of render
+// because of the server-rendering
+if (process.env.NODE_ENV === 'production') {
+    // rehydrate the bundle marks
+    rehydrateMarks().then(() => {
+        ReactDOM.hydrate(app, element);
+    });
+} else {
+    ReactDOM.render(app, element);
+}
 //Enable Hot Module Reloading
 if (module.hot) {
     module.hot.accept();
