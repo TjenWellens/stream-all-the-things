80720365f6c065c2508092af3431c15659476aab
Tjen Wellens <tjen.wellens@gmail.com>
2020-01-19 12:39:27 +0100

    4. Routing

---
+++ app/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Helmet from 'react-helmet-async'
+const About = () => (
+    <React.Fragment>
+        <Helmet>
+            <title>About Page</title>
+        </Helmet>
+        <div>
+            This is the about page
+        </div>
+
+    </React.Fragment>
+)
+export default About
